3e0dad77a1fa3d3b0fb8fd97a4f6ef60e715688d
BlueLiu2004 <blueliu2004@gmail.com>
2024-02-29 15:40:32 +0800

    Add bubble sort

--- HelloGitClone/HelloGitClone/HelloGitClone.cpp
+++ HelloGitClone/HelloGitClone/HelloGitClone.cpp
@@ -7,6 +7,35 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Hello Git Clone!\n";
+    // add a bubble sort
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    std::cout << "original array: \n";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+    std::cout << "\n";
+
+    std::cout << "Sorted array: \n";
+    for (int i = 0; i < n; i++)
+    {
+		std::cout << arr[i] << " ";
+	}
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
